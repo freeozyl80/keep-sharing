142be2974f43d37db5ff2251751fa48012e59a4b
japhy <freeozyl80@gmail.com>
2021-06-27 21:54:31 +0800

    feature: temp save

--- react-demo/bin/build.js
+++ react-demo/bin/build.js
@@ -1,8 +1,18 @@
 const path = require('path')
 const dir = path.resolve(__dirname, '../')
+const cwd = path.resolve(dir, '../')
+const serverDir = path.resolve(cwd, './public')
 
-require('esbuild').build({
+require('esbuild').serve({
+	port: 8888,
+	servedir: serverDir
+}, {
   entryPoints: [path.resolve(dir, './app.jsx')],
   bundle: true,
-  outfile: path.resolve(dir, './dist/out.js'),
-}).catch(() => process.exit(1))
+  outfile: path.resolve(serverDir, './reactApps/out.js')
+}).catch((e) => {
+	console.log(e)
+})
+
+
+
